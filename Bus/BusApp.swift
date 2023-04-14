9d3e1c0f7c83527e4ec5739c0d1456feb98e868e
DarthJarJarJar <88474643+DarthJarJarJar@users.noreply.github.com>
2023-04-14 16:28:52 -0400

    Initial Commit

---
+++ Bus/BusApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusApp.swift
+//  Bus
+//
+//  Created by Ayaan Shahab on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct BusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
